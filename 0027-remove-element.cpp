2f0910ac0f3c82ab9111f2c3e92c151b8c242cd1
kush-kuril <140415031+kush-kuril@users.noreply.github.com>
2026-06-15 22:49:53 +0530

    Update 0027-remove-element.cpp
    Refactor to remove extra vector allocation, optimizing memory footprint to O(1) space (Beats 83.54%)

--- 0027-remove-element.cpp
+++ 0027-remove-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         std::size_t length{nums.size()};
-        std::vector<int> newnums (length);
+        
         std::size_t i{};
         int count {};
         for (const auto& n : nums)
@@ -13,15 +13,12 @@ public:
                 
             }
             else{
-                newnums[i]=n;
+                nums[i]=n;
                 i+=1;
             }
+            
 
         }
-        for (std::size_t t{};t<length;t++)
-        {
-            nums[t]=newnums[t];
-        }
-        return (length-count);
+        return( length-count);
     }
 };
