0477a1d8214920557099c8fc9f4d16b61a43d456
kush-kuril <kushkuril24@gmail.com>
2026-07-05 01:47:40 +0530

    more optimised palindrome

--- easy/9-palindrome-number.cpp
+++ easy/9-palindrome-number.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int dih{x};
+        
         int num{x};
         int count{};
-        
-        if (num<0)
-        {
+
+        if (num < 0) {
             return false;
         }
-        while(num>0)
-        {
-            num/=10;
+        while (num > 0) {
+            num /= 10;
             count++;
         }
-        if (x==0||count==1){return true;}
-        long long int r{};
-        while((x) >0)
-        {
-            r=r*10+(x%10);
-            x=x/10;
-            count--;
-
-        }
-        if (r==dih)
+        num=x;
+        if (x == 0 || count == 1) {
             return true;
-        
+        }
+        long long int r{};
+        while ((x) > 0) {
+            r = r * 10 + (x % 10);
+            x = x / 10;
+            count--;
+        }
+        if (r == num)
+            return true;
+
         return false;
-        
-
-       
-
     }
 };
