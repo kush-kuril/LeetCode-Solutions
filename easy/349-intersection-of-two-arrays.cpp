ad493e80edb1266df657147833c900cc0adc6d39
kush-kuril <kushkuril24@gmail.com>
2026-07-07 23:07:15 +0530

    abit better memory

--- easy/349-intersection-of-two-arrays.cpp
+++ easy/349-intersection-of-two-arrays.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        std::set<int> set1 (nums1.begin(), nums1.end());
-        std::set<int> set2 (nums2.begin(), nums2.end());
+        std::unordered_set<int> set1 (nums1.begin(), nums1.end());
+        std::unordered_set<int> set2 (nums2.begin(), nums2.end());
         std::vector<int> ans{};
         for(const int& i:set1)
         {
