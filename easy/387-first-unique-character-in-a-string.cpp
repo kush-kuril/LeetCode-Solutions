1abceca6535329899af244a587a6ea31791e9100
kush-kuril <kushkuril24@gmail.com>
2026-07-11 11:43:08 +0530

    faster code using array but still aint 0

--- easy/387-first-unique-character-in-a-string.cpp
+++ easy/387-first-unique-character-in-a-string.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        std::unordered_map<char,int> lets {};
-        for(const char& c:s)
-        {
-            if(lets.find(c)!=lets.end())
-            {
-                lets[c]=lets[c]+1;
-            }else{
-                lets[c]=0;
-            }
-            
+        std::array<int, 26> alpha{};
+        int index{};
+        for (const char& c : s) {
+            index = c - 'a';
+            alpha[index]++;
         }
-        for(std::size_t i{};i<s.size();i++)
-        {
-            if(lets[s[i]]==0)
-            {
+        for (std::size_t i{}; i < s.size(); i++) {
+            index = static_cast<char>(s[i]) - 'a';
+            if (alpha[index] == 1) {
                 return static_cast<int>(i);
             }
         }
